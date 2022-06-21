4178d9830d4331ed18a2f8b80c78eb657e7a5dbf
sachinyadav <sachinyadav@cedcoss.com>
2022-06-21 11:31:16 +0530

    register and  login

---
+++ application/app/controllers/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class LoginController extends Controller
+{
+
+    public function indexAction()
+    {
+    }
+    public function welcomeAction()
+    {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $user = Users::findFirstByemail($email);
+        if ($user != null) {
+            if ($user->password == $password) {
+                $message = 'user authenticated';
+                $success = $user->save();
+                $this->view->success = $success;
+            } else {
+                $message = "you are not approved yet";
+                 $this->response->redirect('/login');
+            }
+        } else {
+            $message = 'your are not authenticated ';
+            $this->view->message = $message;
+            $this->response->redirect('/login');
+        }
+        $this->view->message = $message;
+    }
+}
